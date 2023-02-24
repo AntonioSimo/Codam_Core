805cfedf0986b61204f8548c6adc63151d26ecc1
Antonio Simone <asimone@f1r4s4.codam.nl>
2023-02-24 19:15:47 +0100

    update Minitalk

--- Minitalk/Minitalk/server.c
+++ Minitalk/Minitalk/server.c
@@ -6,7 +6,7 @@
 /*   By: asimone <asimone@student.42.fr>              +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/02/13 14:28:42 by asimone       #+#    #+#                 */
-/*   Updated: 2023/02/24 16:24:31 by asimone       ########   odam.nl         */
+/*   Updated: 2023/02/24 18:56:21 by asimone       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,20 @@
 
 void	signal_handler(int sig)
 {
+	static char	c;
+	static int	x;
 
+	c <<= 1;
 	if (sig == SIGUSR1)
-		write(1, "1", 1);
+		c += 1;
 	else if (sig == SIGUSR2)
-		write(1, "0", 1);
+		c += 0;
+	x++;
+	if (x==8)
+	{
+		write(1, &c, 1);
+		x = 0;
+	}
 }
 
 int	main(void)

2d8f5e226c6c2e85ec1de95717b8cf97bf75aed2
Antonio Simone <asimone@f0r4s24.codam.nl>
2023-05-09 19:04:08 +0200

    Beginning Fract-ol

---
+++ Fract-ol/Fract-ol/src/mandelbrot.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*    mandelbrot.c                                      :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: asimone <asimone@student.42.fr>              +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2023/05/09 11:29:19 by asimone       #+#    #+#                 */
+/*   Updated: 2023/05/09 17:12:47 by asimone       ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+#include <math.h>
+
+#define MAX_ITER 1000  // Numero massimo di iterazioni
+#define THRESHOLD 2.0  // Soglia di divergenza
+
+int main(void)
+{
+    printf("PORCO DIO");
+}

56517bf26cf588bee470fea1218e9e4640117e10
Antonio Simone <asimone@f0r4s24.codam.nl>
2023-05-12 17:12:58 +0200

    init Window

--- Fract-ol/Fract-ol/src/mandelbrot.c
+++ Fract-ol/Fract-ol/src/mandelbrot.c
@@ -6,9 +6,17 @@
 /*   By: asimone <asimone@student.42.fr>              +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/05/09 11:29:19 by asimone       #+#    #+#                 */
-/*   Updated: 2023/05/12 15:29:53 by asimone       ########   odam.nl         */
+/*   Updated: 2023/05/12 15:53:11 by asimone       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 
+int32_t main(void)
+{
+	mlx_t	*mlx;
+
+	mlx = mlx_init(1920, 1080, "Hello World!", 1);
+	mlx_loop(mlx);
+	mlx_terminate(mlx);
+}

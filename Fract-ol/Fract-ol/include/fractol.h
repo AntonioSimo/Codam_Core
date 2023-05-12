9e431079c9aae58cb4fde725ecb942880f629ed0
Antonio Simone <asimone@f0r4s24.codam.nl>
2023-05-12 14:08:09 +0200

    Edit Makefile

--- Fract-ol/Fract-ol/include/fractol.h
+++ Fract-ol/Fract-ol/include/fractol.h
@@ -6,11 +6,16 @@
 /*   By: asimone <asimone@student.42.fr>              +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/05/09 14:43:25 by asimone       #+#    #+#                 */
-/*   Updated: 2023/05/09 14:44:20 by asimone       ########   odam.nl         */
+/*   Updated: 2023/05/10 12:52:15 by asimone       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FRACTOL_H
 # define FRACTOL_H
+
+# include "../lib/Libft/include/libft.h"
+# include "../lib/MLX42/include/MLX42/MLX42.h"
+#include <math.h>
+
 
 #endif
--- Fract-ol/Fract-ol/src/mandelbrot.c
+++ Fract-ol/Fract-ol/src/mandelbrot.c
@@ -1,22 +1,14 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        ::::::::            */
-/*    mandelbrot.c                                      :+:    :+:            */
+/*   mandelbrot.c                                       :+:    :+:            */
 /*                                                     +:+                    */
 /*   By: asimone <asimone@student.42.fr>              +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/05/09 11:29:19 by asimone       #+#    #+#                 */
-/*   Updated: 2023/05/09 17:12:47 by asimone       ########   odam.nl         */
+/*   Updated: 2023/05/10 12:53:28 by asimone       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-#include <math.h>
+#include "fractol.h"
 
-#define MAX_ITER 1000  // Numero massimo di iterazioni
-#define THRESHOLD 2.0  // Soglia di divergenza
-
-int main(void)
-{
-    printf("PORCO DIO");
-}

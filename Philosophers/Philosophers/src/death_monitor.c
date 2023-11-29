e8556a0ab3a47f864b9ea31f3c51ddfad75ce8ee
Antonio Simone <asimone@f1r5s5.codam.nl>
2023-11-29 19:34:59 +0100

    Norminette pt.1

--- Philosophers/Philosophers/src/death_monitor.c
+++ Philosophers/Philosophers/src/death_monitor.c
@@ -6,23 +6,39 @@
 /*   By: asimone <asimone@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/14 16:17:00 by asimone           #+#    #+#             */
-/*   Updated: 2023/11/29 17:33:34 by asimone          ###   ########.fr       */
+/*   Updated: 2023/11/29 19:26:44 by asimone          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/philo.h"
 
-void		*check_monitor(void *args)
+static void	*helper_check(t_philo *philo, t_data *data, int i)
 {
-	t_data *data;
+	int	delta_time;
+	
+	pthread_mutex_lock(&philo[i].mut_eat_t);
+	delta_time = get_current_time() - philo[i].last_eat_time;
+	pthread_mutex_unlock(&philo[i].mut_eat_t);
+	if (delta_time >= data->time_to_die)
+	{
+		die_time(&philo[i], data);
+		return (NULL);
+	}
+	return (NULL);
+}
+
+void	*check_monitor(void *args)
+{
+	t_data	*data;
+	t_philo	*philo;
+	int		philo_fully_ate;
+	int		max_meals;
+	int		i;
+	int		n_meals;
 
 	data = (t_data *)args;
-	t_philo	*philo;
-	int	i;
-	int delta_time;
-	int n_meals;
-	int philo_fully_ate = 0;
-	int max_meals = data->nb_meals;
+	philo_fully_ate = 0;
+	max_meals = data->nb_meals;
 	philo = data->philos;
 	while (1)
 	{
@@ -36,16 +52,7 @@ void		*check_monitor(void *args)
 			if (data->nb_meals && n_meals == max_meals)
 				philo_fully_ate++;
 			else
-			{
-				pthread_mutex_lock(&philo[i].mut_eat_t);
-				delta_time = get_current_time() - philo[i].last_eat_time;
-				pthread_mutex_unlock(&philo[i].mut_eat_t);
-				if (delta_time >= data->time_to_die)
-				{
-					die_time(&philo[i], data);
-					return (NULL);
-				}
-			}
+				helper_check(philo, data, i);
 			i++;
 		}
 		if (philo_fully_ate == data->num_of_philos)
